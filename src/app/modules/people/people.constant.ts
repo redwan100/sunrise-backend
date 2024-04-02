12a2a247817e2f487a721324562e1d8bdf62155b
Redwan islam <redwanislam2760@gmail.com>
2024-04-02 22:07:50 +0600

    added reuseable query builder for filter, sort,paginate

---
+++ src/app/modules/people/people.constant.ts
@@ -0,0 +1,1 @@
+export const peopleSearchableFields = ["name.firstName", "name.lastName"];
--- src/app/modules/people/people.service.ts
+++ src/app/modules/people/people.service.ts
@@ -1,41 +1,17 @@
+import QueryBuilder from "../../builder/QueryBuilder";
+import { peopleSearchableFields } from "./people.constant";
 import People from "./people.model";
 
 const getAllPeopleFromDB = async (query: Record<string, unknown>) => {
-  let search = "";
-  const limit: number = Number(query?.limit) || 10;
-  const page = query?.page || 1;
+  const peopleQuery = new QueryBuilder(People.find(), query)
+    .search(peopleSearchableFields)
+    .filter()
+    .sort()
+    .paginate()
+    .fields();
+  const result = await peopleQuery.modelQuery;
 
-  if (query.searchTerm) {
-    search = query.searchTerm as string;
-  }
-
-  const queryObj = { ...query };
-
-  const peopleSearchQuery = ["name.firstName", "name.lastName"];
-
-  const searchQuery = People.find({
-    $or: peopleSearchQuery.map((field) => ({
-      [field]: {
-        $regex: search,
-        $options: "i",
-      },
-    })),
-  });
-
-  const excludeFields = ["searchTerm", "page", "limit", "sort"];
-  excludeFields.forEach((field) => delete queryObj[field]);
-
-  const filterQuery = searchQuery.find(queryObj).populate("user");
-
-  let sort = "-createdAt";
-  if (query.sort) {
-    sort = query.sort as string;
-  }
-
-  const sortQuery = filterQuery.sort(sort);
-  const limitQuery = await sortQuery.limit(limit);
-
-  return limitQuery;
+  return result;
 };
 
 export const PeopleService = {
